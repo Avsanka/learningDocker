110d47e519c2d834c069586e2c68d99ce77aeecc
Avsanka <nikita555534@gmail.com>
2025-02-09 21:01:28 +0600

    fix2

--- 10.py
+++ 10.py
@@ -13,4 +13,4 @@ def giveNum():
     return str(randint(1, 100))
 
 if __name__ == '__main__':
-    app.run(debug=True, host='127.0.0.1', port=8081)
+    app.run(debug=True, host='0.0.0.0', port=8081)
